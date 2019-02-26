00e6bdfb0bf16bbb7ee1078de8da38b2dee7c0f6
Antoine Potdevin <antoinepotdevin@gmail.com>
2019-02-26 12:12:22 +0100

    Typo routes corrected for you Xavier

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :profiles, only: [:show, :new, :create, :update] do 
-    resources :performances, only: [:show, :new, :create, :edit, :update] do 
+  resources :profiles, only: %i[show, new, create, update] do 
+    resources :performances, only: %i[show, new, create, edit, :update] do 
     end
-    resources :performance_dates, only: [:new, :create, :update, :destroy] do
+    resources :performance_dates, only: %i[new, create, update, destroy] do
     end 
   end 
-  resources :booking, only: [:new, :create]
+  resources :booking, only: %i[new, create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
