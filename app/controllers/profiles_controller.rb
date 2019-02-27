f01e9fe2eba7f7fa37b57cfc8a5ed3d3822c4060
Xavier Pichetti <pichon.pich@free.fr>
2019-02-27 16:47:24 +0100

    ajout du nombre de performance

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
   def show
     @profile = current_user.profile
     @performances = Performance.where(profile_id: @profile.id)
+    @nbr_performances = @performances.size
   end
 end
