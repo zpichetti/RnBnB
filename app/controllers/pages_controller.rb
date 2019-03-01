4b80e274e4b0107c73da014cf5068f9b10557299
Antoine Potdevin <antoinepotdevin@gmail.com>
2019-03-01 16:25:19 +0100

    Implementing search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def home
+    if params[:query].present?
     @performances = Performance.where(active: true)
+  end
+ends
+
+
+def index
+  if params[:query].present?
+    sql_query = "title ILIKE :query OR description ILIKE :query"
+    @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+  else
+    @movies = Movie.all
   end
 end
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -8,4 +8,11 @@ class Performance < ApplicationRecord
   belongs_to :category
   belongs_to :profile
   has_many :booking
+
+  include PgSearch
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true } 
+    }
 end
